24b28fb9cb36ac36c86bce3173294a4d83c7a69d
DBanda <danielbanda.stg@gmail.com>
2025-04-29 21:15:35 -0600

    Inscripciones cerradas

--- JS/main.js
+++ JS/main.js
@@ -17,5 +17,17 @@ const actualizarContador = () => {
     `${dias}d ${horas}h ${minutos}m ${segundos}s`;
 };
 
-const fechaLimite = new Date("May 2, 2025 22:00:00").getTime();
+// Configura la fecha límite de la cuenta atrás
+const fechaLimite = new Date("Apr 29, 2025 21:03:11").getTime();
+
+// Ejecuta el contador cada segundo
 const intervalo = setInterval(actualizarContador, 1000);
+
+// Añadir aquí código para forzar el "Inscripciones cerradas" si quieres hacer pruebas:
+const fechaLimitePasada = new Date("Apr 1, 2024 00:00:00").getTime(); // Para prueba
+
+// Si estás probando y quieres forzar el "Inscripciones cerradas" sin esperar, descomenta la siguiente línea:
+if (new Date().getTime() > fechaLimite) {
+  clearInterval(intervalo);
+  document.getElementById("contador").innerHTML = "Inscripciones cerradas";
+}
